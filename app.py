f73258d0befd5d66dcfe7a40fbb58f98a5ae3dc5
Emmanuel Oni <60719635+username2512@users.noreply.github.com>
2025-01-30 19:37:55 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, Response
+import cv2
+from pyzbar.pyzbar import decode
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            decoded_objects = decode(frame)
+            for obj in decoded_objects:
+                points = obj.polygon
+                if len(points) > 4:
+                    hull = cv2.convexHull(np.array([point for point in points], dtype=np.float32))
+                    hull = list(map(tuple, np.squeeze(hull)))
+                else:
+                    hull = points
+                n = len(hull)
+                for j in range(n):
+                    cv2.line(frame, hull[j], hull[(j + 1) % n], (255, 0, 0), 3)
+
+                data = obj.data.decode('utf-8')
+                if data == 'https://ar-frame.vercel.app/sam.mp4':
+                    # Play video or perform any action
+                    pass
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    cap.release()
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
